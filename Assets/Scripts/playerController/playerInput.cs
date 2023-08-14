2e6eca080e24552e255b37a4521b39cf8e24ca09
Megabanzo <timkern42@gmail.com>
2023-08-14 15:16:43 +1200

    Added basic Up and down movement using getKey

--- Assets/Scripts/playerController/playerInput.cs
+++ Assets/Scripts/playerController/playerInput.cs
@@ -1,21 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class playerInput : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public KeyCode UpKey = KeyCode.UpArrow; // Keycode for up
+    public KeyCode DownKey = KeyCode.DownArrow; // Keycode for up
+    public float MoveSpeed = 5.0f; // The force to apply for the jump
+
+    private Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetAxis("Vertical") > 0)
+
+        bool up = Input.GetKey(UpKey);
+        bool down = Input.GetKey(DownKey);
+        // Check if the jump key is pressed
+        if (up || down)
         {
-            transform.position = transform.position + new Vector3(0,1,0);
+            // Apply the jump force to the character's y velocity
+            Vector2 velocity = rb.velocity;
+
+            if (up && !down)
+            {
+                velocity.y = MoveSpeed;
+            }else if(down && !up)
+            {
+                velocity.y = -MoveSpeed;
+            }
+   
+
+            rb.velocity = velocity;
         }
+
+        // Check if the jump key is pressed
+        //if (Input.GetKey(DownKey))
+        //{
+        //    // Apply the jump force to the character's y velocity
+        //    Vector2 velocity = rb.velocity;
+        //    velocity.y = -MoveSpeed;
+        //    rb.velocity = velocity;
+        //}
     }
 }
